ac4a965a21462c1027364a47550d81611fc86eb0
Mick-Raisey <tabs4bass@gmail.com>
2025-07-06 22:48:50 +0930

    added exercises 203 to 208

---
+++ ch2/ex205.py
@@ -0,0 +1,9 @@
+# 2-5. Famous Quote:
+# Find a quote from a famous person.
+# Print the quote and the name of its author.
+
+famous_person = "Ace Frehley"
+quote = "No, actually I'm a plumber"
+
+message = f"{famous_person} once said, \"{quote}'"
+print(message)
