0c36c52ae36b99b82f3fd18120ba934a763d23f4
Mick-Raisey <tabs4bass@gmail.com>
2025-07-07 12:13:11 +0930

    Added exercises 301 302 and 303

---
+++ ch3/ex302.py
@@ -0,0 +1,10 @@
+# 3-2. Greetings:
+# Start with the list you used in Exercise 3-1, but instead of just
+# printing each person’s name, print a message to them.
+# The text of each message should be the same,
+# but each message should be personalized with the person’s name.
+
+names = ["mimi", "jacinta", "nugget"]
+print(f"Hello {names[0].title()}")
+print(f"Hello {names[1].title()}")
+print(f"Hello {names[2].title()}")
---
+++ ch3/ex303.py
@@ -0,0 +1,14 @@
+# 3-3. Your Own List:
+# Think of your favorite mode of transportation, such as a
+# motorcycle or a car, and make a list that stores several examples. Use your list
+# to print a series of statements about these items, such as
+# “I would like to own a Honda motorcycle.”
+
+vehicles = ["mustang", "harley davidson", "maserati"]
+
+message = f"I would like to drive a {vehicles[0].title()}"
+print(message)
+message = f"I would like to ride a {vehicles[1].title()}"
+print(message)
+message = f"I would like to drive a {vehicles[2].title()}"
+print(message)
