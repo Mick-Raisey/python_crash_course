260817d0c2f51e40950838643c6900fe63c05b94
Mick-Raisey <tabs4bass@gmail.com>
2025-07-09 12:24:17 +0930

    added exercise 401 and 402

---
+++ ch4/ex401.py
@@ -0,0 +1,11 @@
+# 4-1. Pizzas (Bands):
+# Think of at least three kinds of your favorite pizza.
+# Store these pizza names in a list,
+# and then use a for loop to print the name of each pizza.
+
+bands = ["ac/dc", "kiss", "iron maiden"]
+
+for band in bands:
+    print(f"{band.upper()} rocks!")
+
+print("These band Rock! \m/")
---
+++ ch4/ex402.py
@@ -0,0 +1,11 @@
+# 4-2. Animals:
+# Think of at least three different animals that have a common characteristic.
+# Store the names of these animals in a list, and then use a for loop to
+# print out the name of each animal.
+
+animals = ["dog", "cat", "horse"]
+
+for animal in animals:
+    print(f"{animal}'s have four legs")
+
+print("\nAny of these animals would make a great pet")
