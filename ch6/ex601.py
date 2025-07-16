d824f3b128fdfc652fbf9230dfffde379387ba5b
Mick-Raisey <tabs4bass@gmail.com>
2025-07-16 19:54:35 +0930

    Added exercies 601 to 603

---
+++ ch6/ex601.py
@@ -0,0 +1,17 @@
+# 6-1. Person:
+# Use a dictionary to store information about a person you know.
+# Store their first name, last name, age, and the city in which they live.
+# You should have keys such as first_name, last_name, age, and city.
+# Print each piece of information stored in your dictionary.
+
+person = {
+    "first_name": "mick",
+    "last_name": "rayzee",
+    "age": 58,
+    "city": "adelaide",
+}
+
+print(person["first_name"].title())
+print(person["last_name"].title())
+print(person["age"])
+print(person["city"].title())
---
+++ ch6/ex602.py
@@ -0,0 +1,19 @@
+# 6-2. Favorite Numbers:
+# Use a dictionary to store people’s favorite numbers.
+# Think of five names, and use them as keys in your dictionary.
+# Think of a favorite number for each person, and store each as a value in your dictionary.
+# Print each person’s name and their favorite number.
+
+favourite_numbers = {
+    "mick": 13,
+    "viv": 5,
+    "jessica": 32,
+    "nugget": 7,
+    "jasmine": 12,
+}
+
+print(f"Mick's favourite number is {favourite_numbers['mick']}")
+print(f"Viv's favourite number is {favourite_numbers['viv']}")
+print(f"Jessica's favourite number is {favourite_numbers['jessica']}")
+print(f"Nugget's favourite number is {favourite_numbers['nugget']}")
+print(f"Jasmine's favourite number is {favourite_numbers['jasmine']}")
