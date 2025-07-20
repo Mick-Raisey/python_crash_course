c41be10fc6f7c3dfaec3c841dacf2ca0b725a645
Mick-Raisey <tabs4bass@gmail.com>
2025-07-20 22:36:43 +0930

    Added exercise 801 and 802

---
+++ ch8/ex802.py
@@ -0,0 +1,12 @@
+# 8-2. Favorite Book: Write a function that accepts one
+# parameter, title. The function should print a message, such as One of my
+# favorite books is Alice in Wonderland. Call the function, making sure to
+# include a book title as an argument in the function call.
+
+
+def favourite_artist(artist):
+    print(f"One of my favourite artists is {artist.upper()}")
+
+
+favourite_artist("ac/dc")
+favourite_artist("kiss")
